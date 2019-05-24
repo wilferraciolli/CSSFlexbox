eed41bdbfcb0f9e8db9e37ee314b472d9a5d0375
Wiliam Ferraciolli <wiliam334@hotmail.com>
2019-05-24 15:16:51 +0100

    Created javascript files to control css properties if clicked

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
+    //Create function to happen when clicked on the magnify item on the html
+    $("#magnify").click(function() {
+       //hides the magnify div
+        $(this).hide();
 
+        //display the search bar
+        $("#search").toggle("scale");
+
+        //change the css assign to the search bar
+        $("#search").css("display", "block;");
+    });
 });
 
